ea002ddf9e24ab051aff434f5c2a4bf28b064597
sai <3nadhananth@gmail.com>
2023-02-24 20:53:53 +0530

    first commit

---
+++ src/main/java/com/pixeltrice/springbootOTPenabledapp/SpringBootOtpEnabledAppApplication.java
@@ -0,0 +1,17 @@
+package com.pixeltrice.springbootOTPenabledapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class SpringBootOtpEnabledAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOtpEnabledAppApplication.class, args);
+	}
+
+}
